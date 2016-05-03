ab17c1bc98621e57e3bbddfde385d6fbecac7f66
yohann_git_hub <y.caillon-morisseau@dreamcentury.com>
2016-05-03 10:44:37 +0200

    correction 404

--- lib/OCFram/HTTPResponse.php
+++ lib/OCFram/HTTPResponse.php
@@ -19,8 +19,8 @@ class HTTPResponse extends ApplicationComponent
  
   public function redirect404()
   {
-      this->page = new Page($this->app);
-      $this->page->setContentFile(__DIR__.'/../../Errors/404.html');
+      $this->page = new Page($this->app);
+      $this->page->setContentFile(__DIR__.'/../../Errors/404.php');
       $this->addHeader('HTTP/1.0 404 Not Found');
  
       $this->send();
